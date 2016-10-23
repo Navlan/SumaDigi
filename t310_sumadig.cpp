9167a158f2513c81e403d9bda89bf31956d7d76c
Alexander <alexrubiom56@hotmail.com>
2016-10-23 02:27:55 +0200

    SumaDigi
    Versión para el juez

---
+++ t310_sumadig.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int pedirNumPos();
+int sumaDig(int num);
+
+int pedirNumPos()
+{
+	int num;
+	cout << "Introduce un numero positivo." << endl;
+	cin >> num;
+	while (num <= 0)
+	{
+		cout << "Introduce un numero positivo." << endl;
+		cin >> num;
+	}
+	return num;
+}
+
+int sumaDig(int num)
+{
+	int suma = 0;
+	while (num > 0)
+	{
+		suma = suma + (num % 10);
+		num = num / 10;
+	}
+	return suma;
+}
+
+int main()
+{
+	int num = pedirNumPos();
+	cout << "La suma de los digitos de " << num << " es " << sumaDig(num) << "." << endl;
+	return 0;
+}
